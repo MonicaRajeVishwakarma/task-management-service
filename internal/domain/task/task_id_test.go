8e364e9f37a8c7a789c07e8c4c0a224301a07ca3
MonicaRajeVishwakarma <43462756+MonicaRajeVishwakarma@users.noreply.github.com>
2026-08-06 12:35:44 +0200

    Add TestTaskIDFromString tests

--- internal/domain/task/task_id_test.go
+++ internal/domain/task/task_id_test.go
@@ -56,3 +56,54 @@ func TestTaskID_Equals(t *testing.T) {
 		t.Fatal("expected different TaskIDs to not be equal")
 	}
 }
+func TestTaskIDFromString(t *testing.T) {
+	t.Parallel()
+
+	strUUID := "07cd2aba-8e5d-4827-ac43-66274ec4a2f4"
+	expectedUUID := uuid.MustParse(strUUID)
+
+	tests := []struct {
+		name string // description of this test case
+		// Named input parameters for target function.
+		id      string
+		want    TaskID
+		wantErr bool
+	}{
+		{
+			name : "successfully returns the TaskID ",
+			id : strUUID,
+			want : TaskID{value: expectedUUID},
+			wantErr: false,
+		},
+		{
+			name : "rejects invaild UUID",
+			id : "INVALID",
+			want : TaskID{},
+			wantErr: true,
+		},
+		{
+			name: "rejects empty string",
+			id: "",
+			want: TaskID{},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, gotErr := TaskIDFromString(tt.id)
+			if gotErr != nil {
+				if !tt.wantErr {
+					t.Errorf("TaskIDFromString() failed: %v", gotErr)
+				}
+				return
+			}
+			if tt.wantErr {
+				t.Fatal("TaskIDFromString() succeeded unexpectedly")
+			}
+
+			if got.value != tt.want.value{
+				t.Errorf("TaskIDFromString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
