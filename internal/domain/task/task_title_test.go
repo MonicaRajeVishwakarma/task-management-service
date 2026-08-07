546d4288d91eac0b89e0cf715e4449bccf9c7acf
MonicaRajeVishwakarma <43462756+MonicaRajeVishwakarma@users.noreply.github.com>
2026-08-08 00:20:14 +0200

    Add TaskTitle string representation and equality

--- internal/domain/task/task_title_test.go
+++ internal/domain/task/task_title_test.go
@@ -1,6 +1,8 @@
 package task
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestNewTaskTitle(t *testing.T) {
 	t.Parallel()
@@ -45,9 +47,40 @@ func TestNewTaskTitle(t *testing.T) {
 				t.Fatal("error while creating taskTitle")
 			}
 
-			if got.String() != tt.want {
+			if got.value != tt.want {
 				t.Errorf("error while creating taskTitle, got %v ,want %v", got.value, tt.want)
 			}
 		})
+	}
+}
+
+// TestTaskTitle_String provides the String value of the TestTitle
+func TestTaskTitle_String(t *testing.T) {
+	t.Parallel()
+
+	inputTitle := TaskTitle{
+		value: "Title",
+	}
+	expectedTitle := "Title"
+
+	if inputTitle.String() != expectedTitle {
+		t.Errorf("inputTitle %v is not matching with expected title %v ", inputTitle, expectedTitle)
+	}
+}
+
+// TestTaskTitle_String compares the titles and return equality results
+func TestTaskTitle_Equals(t *testing.T){
+	t.Parallel()
+
+	title := TaskTitle{
+		value: "Title",
+	}
+
+	otherTitle := TaskTitle{
+		value: "Title",
+	}
+
+	if !title.Equals(otherTitle){
+		t.Errorf("title %v and otherTitle %v are not equal",title,otherTitle)
 	}
 }
